0a247781738a3672245b00ad90d1e58828b28106
Esuyawkal1 <esuyawkal.fentahun@gmail.com>
2026-05-17 14:32:58 +0300

    Add flag.h header file with rendering callbacks

---
+++ flag.h
@@ -0,0 +1,12 @@
+#ifndef FLAG_H
+#define FLAG_H
+
+// Core rendering callbacks
+void display();
+void reshape(int w, int h);
+
+// Flag system helpers
+void drawFlagBackground(float width, float height);
+void drawSunWrapper(float width, float height);
+
+#endif
