065696e08cd9250584d8170142e9e895ab50cd08
Esuyawkal1 <esuyawkal.fentahun@gmail.com>
2026-05-18 13:13:43 +0300

    Add sunRotation variable to flag.h

--- flag.h
+++ flag.h
@@ -1,11 +1,9 @@
 #ifndef FLAG_H
 #define FLAG_H
 
-// Core rendering callbacks
+extern float sunRotation;
 void display();
 void reshape(int w, int h);
-
-// Flag system helpers
 void drawFlagBackground(float width, float height);
 void drawSunWrapper(float width, float height);
 
